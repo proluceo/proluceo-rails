417f13c441c60c8188c958d922d3fe3c7464ffe7
Bruno Enten <bruno@enten.me>
2023-04-19 11:21:22 +0300

    documents inbox complete

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -1,5 +1,5 @@
 class JournalEntriesController < ApplicationController
-  before_action :set_journal_entry, only: %i[ show edit update destroy ]
+  before_action :set_journal_entry, only: %i[show edit update destroy]
 
   # GET /journal_entries
   def index
@@ -7,8 +7,7 @@ class JournalEntriesController < ApplicationController
   end
 
   # GET /journal_entries/1
-  def show
-  end
+  def show; end
 
   # GET /journal_entries/new
   def new
@@ -16,15 +15,14 @@ class JournalEntriesController < ApplicationController
   end
 
   # GET /journal_entries/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /journal_entries
   def create
     @journal_entry = JournalEntry.new(journal_entry_params)
 
     if @journal_entry.save
-      redirect_to @journal_entry, notice: "Journal entry was successfully created."
+      redirect_to @journal_entry, notice: 'Journal entry was successfully created.'
     else
       render :new, status: :unprocessable_entity
     end
@@ -33,7 +31,7 @@ class JournalEntriesController < ApplicationController
   # PATCH/PUT /journal_entries/1
   def update
     if @journal_entry.update(journal_entry_params)
-      redirect_to @journal_entry, notice: "Journal entry was successfully updated."
+      redirect_to @journal_entry, notice: 'Journal entry was successfully updated.'
     else
       render :edit, status: :unprocessable_entity
     end
@@ -42,17 +40,20 @@ class JournalEntriesController < ApplicationController
   # DELETE /journal_entries/1
   def destroy
     @journal_entry.destroy
-    redirect_to journal_entries_url, notice: "Journal entry was successfully destroyed."
+    redirect_to journal_entries_url, notice: 'Journal entry was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_journal_entry
-      @journal_entry = JournalEntry.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def journal_entry_params
-      params.fetch(:journal_entry, {})
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_journal_entry
+    @journal_entry = JournalEntry.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def journal_entry_params
+    params.fetch(:journal_entry, {})
+  end
+
+  include CompanyDependent
 end
