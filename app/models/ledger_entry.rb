27b6403d04277ebfd5276730b0637596bf1c2edf
Bruno Enten <bruno@enten.me>
2023-06-13 02:06:48 +0300

    create ledger entry

--- app/models/ledger_entry.rb
+++ app/models/ledger_entry.rb
@@ -1,2 +1,11 @@
 class LedgerEntry < ApplicationRecord
+  self.primary_keys = :ledger_entry_id, :account_number
+  belongs_to :company
+  belongs_to :account, class_name: 'Account', foreign_key: [:company_id, :account_number]
+
+  def self.create_from_parts(parts)
+    new_entry_uuid =  Digest::UUID.uuid_v4
+    parts.each { |p| p[:ledger_entry_id] = new_entry_uuid }
+    insert_all(parts)
+  end
 end
